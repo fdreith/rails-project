2e7bbd0f4d32aeef68151477ca54b11c921a809a
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-03-17 14:09:07 -0700

    generate resource book

---
+++ db/migrate/20200317210837_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.string :genre
+      t.integer :page_count
+
+      t.timestamps
+    end
+  end
+end
