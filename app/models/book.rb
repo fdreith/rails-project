ee42315b48bfa67caeef603a7cec8a58f69dba43
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-03-17 20:48:39 -0700

    book associations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+  has_many :users, through: :user_books
+  has_many :book_clubs
+  # has_many :favorited_quotes
+  # has_many :annotations
+  # has_many :recommendations
 end
