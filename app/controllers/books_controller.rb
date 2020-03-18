31a8aead1b80bbde376d1d0ba26248d95f86f82c
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-03-18 08:05:48 -0700

    new method

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,15 @@
 class BooksController < ApplicationController
+
+  def index 
+  end
+
+  def new
+    @book = Book.new
+    @authors = Author.all
+    @genres = Genre.all
+  end
+
+  def show
+  end
+
 end
