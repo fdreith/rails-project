e8608db0e1a3f46cb5a6580ad05d36410ba75037
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-03-18 13:46:16 -0700

    devise :users before :users resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :comments
   resources :user_book_clubs
   resources :book_clubs
   resources :authors
   resources :genres
   resources :user_books
+  resources :users
   resources :books
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root "home#index"
-
-  devise_for :users, controllers: { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
 end
