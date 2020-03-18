f8bf7fb7863b8390bd2db6f64027a0c68884cc92
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-03-17 20:22:31 -0700

    Changing username to name on users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_211243) do
+ActiveRecord::Schema.define(version: 2020_03_17_222956) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2020_03_17_211243) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
