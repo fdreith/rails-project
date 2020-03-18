8ff3463ab3735e8b672623c3abb4f34933e43f47
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-03-17 20:27:18 -0700

    OAuth set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   resources :user_books
   resources :books
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root "home#index"
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
 end
