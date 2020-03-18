ea072a03f9798591ca6f8e2fc64ddff5c71e3f04
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-03-18 08:21:40 -0700

    Owner association

--- app/models/book_club.rb
+++ app/models/book_club.rb
@@ -3,6 +3,7 @@ class BookClub < ApplicationRecord
   has_many :users, through: :user_book_clubs
   belongs_to :book
   has_many :comments
+  belongs_to :owner, class_name: "User"
 
   validates :name, presence: true, uniqueness: true 
   validates :book_id, presence: true
