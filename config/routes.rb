448095ffafb1b0d2421ac18bc7c1312d7c2f714e
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-03-19 18:17:58 -0700

    reorganized routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,27 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  resources :user_book_clubs
+
   resources :book_clubs, only: [:show] do 
     resources :comments, only: [:index, :new]
   end
-  resources :comments
+
   resources :authors, only: [:show] do 
-    resources :books, only: [:index, :show]
+    resources :books, only: [:index, :show, :new]
   end
+
   resources :genres, only: [:show] do 
     resources :books, only: [:index, :show]
   end
+
   resources :user_books
+  resources :authors
+  resources :genres
+  resources :book_clubs
   resources :users
   resources :books
+  resources :comments
+  resources :user_book_clubs
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
