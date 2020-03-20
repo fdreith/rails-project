7d1af9f3f43a3f3899718c0344ec9080a5590029
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-03-19 19:09:07 -0700

    Changing location of the homepage and root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root "static#home"
+
   devise_for :users, controllers: { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
 
 
@@ -25,5 +28,4 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root "home#index"
 end
